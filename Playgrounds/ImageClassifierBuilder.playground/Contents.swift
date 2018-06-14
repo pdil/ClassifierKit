6d2f5e4e99c311a89c46a9050dc12acb873576df
Paolo Di Lorenzo <dilorenzopl@gmail.com>
2018-06-13 20:39:36 -0400

    Finish adding image builder instructions

--- Playgrounds/ImageClassifierBuilder.playground/Contents.swift
+++ Playgrounds/ImageClassifierBuilder.playground/Contents.swift
@@ -1,4 +1,4 @@
-/*: some text
+/*: Explanation
 
  # Image Classifier Builder
 
@@ -8,12 +8,23 @@
 
  ## Presenting the live view
 
- To present the interface for training the model from within the Playground, first run `builder.showInLiveView()` by click the play arrow at the beginning of the line:
- ![Show Builder](show-builder.jpeg)
+ To present the interface for training the model from within the Playground, first run `builder.showInLiveView()` by click the play arrow at the beginning of the line.
 
- Next, open the Assistant Editor to see the Live View:
- ![Open Assistant Editor]()
+ Next, open the Assistant Editor (⌥ ⌘ ↩︎) to see the Live View.
 
+ ## Training the model
+
+ Drag the "Training Data" folder from the Resources folder to the "Drop Images to Begin Testing" section of the Live View.
+
+ `Create ML` will begin training the model using the images.
+
+ ## Testing the model
+
+ Drag the "Testing Data" folder from the Resources folder to the Live View once again to test the model.
+
+ ## Saving the trained model
+
+ If the testing results are satisfactory, the trained model can either be saved to your local computer as a `.mlmodel` file or dragged directly into an Xcode project to quickly be implemented into an app.
 
 */
 
